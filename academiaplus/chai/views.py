4c2b832f031854f7eda8efa1e0277c73338079d0
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-08-19 13:11:01 +0545

    'updated

---
+++ academiaplus/chai/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import ChaiVarity
+
+# Create your views here.
+
+def all_chai(request):
+    chais = ChaiVarity.objects.all()
+    return render(request, 'chai/all_chai.html',{'chais':'chais'})
